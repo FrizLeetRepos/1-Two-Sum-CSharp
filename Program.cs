dea57a38c1b7e0ccde31f0bb9cf1aef6d7e84b7c
frizzle101101 <aaronfritz96@gmail.com>
2024-11-04 14:43:09 -0500

    Commit Solution

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace _1_Two_Sum {
+    internal class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+            //solution.TwoSum()
+        }
+    }
+}
